fb8986035808e03821736f4ddd0174cbf581931a
MoonlitCode <1091690269@qq.com>
2025-05-26 21:28:07 +0800

    master-02-commit

--- src/main/java/com/moonlit/git/GitTest.java
+++ src/main/java/com/moonlit/git/GitTest.java
@@ -2,6 +2,7 @@ package com.moonlit.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Welcome to Git!");
+        System.out.println("Welcome to Git 01!");
+        System.out.println("Welcome to Git 02!");
     }
 }
