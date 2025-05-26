967f03976814fe5a1d126f7b830eebd377a37545
MoonlitCode <1091690269@qq.com>
2025-05-26 21:43:28 +0800

    hot-fix-02-commit

--- src/main/java/com/moonlit/git/GitTest.java
+++ src/main/java/com/moonlit/git/GitTest.java
@@ -2,10 +2,15 @@ package com.moonlit.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        // common
+        System.out.println("hot-fix:Welcome to Git 01!");
+
+        // master
         System.out.println("Welcome to Git 01!");
         System.out.println("Welcome to Git 02!");
         System.out.println("Welcome to Git 03!");
 
+        // hot-fix
         System.out.println("Welcome to Git 01!");
     }
 }
