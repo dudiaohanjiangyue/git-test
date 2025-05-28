90154a9051cd52fad1446fb8ca7927e0db107f95
MoonlitCode <1091690269@qq.com>
2025-05-28 22:03:19 +0800

    dev-yq-02-commit-01

--- src/main/java/com/moonlit/git/GitTest.java
+++ src/main/java/com/moonlit/git/GitTest.java
@@ -13,5 +13,8 @@ public class GitTest {
 
         // hot-fix
         System.out.println("Welcome to Git 01!");
+
+        // dev-yq-02
+        System.out.println("dev-yq-02:Welcome to Git 01!");
     }
 }
